ad08a129642754475bd7ecdd9ef05d6d206018b2
Radu Simen <radu103@hotmail.com>
2021-11-22 18:28:36 +0200

    typo

--- demo/src/main/java/com/example/web/app/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/web/app/demo/controller/DemoController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
 @RequestMapping("demo")
 public class DemoController {
