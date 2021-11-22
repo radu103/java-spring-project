c192f8417b8f8def3eb85333f57e661c68c20838
Radu Simen <radu103@hotmail.com>
2021-11-22 17:46:37 +0200

    working hello world

---
+++ demo/src/main/java/com/example/web/app/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.example.web.app.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+    
+    @GetMapping(value="hello")
+    public String HelloWorld() {
+        return "Hello World";
+    }
+}
